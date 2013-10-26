d10104c05a3060409c378362e0c931511c676380
Josh LeBlanc <jleblan8@unb.ca>
2013-10-26 04:09:48 -0300

    Fixed bug where player would stop on flat ground. Fixed bug where jumping against a wall would temporarily transform user into a rocket propelled box of wonders.

--- main.lua
+++ main.lua
@@ -7,8 +7,6 @@ atl.Loader.path = 'lib/'
 local map = atl.Loader.load("test.tmx")
 local tx, ty = 0, 0
 tileSize = 32
-numColl = 0
-canJump = true
 function love.draw()
 	
 	love.graphics.translate(math.floor(tx), math.floor(ty))
@@ -16,16 +14,18 @@ function love.draw()
 	map:draw()
 
 	objects.player:draw()
+	love.graphics.setColor(255,255,255)
+	love.graphics.print(tostring(objects.player.canJump), 0,0)
 	
 end
 function love.update(dt)
 	world:update(dt)
-	player = objects.player
-	pGridCoords = player:getGridCoords()
+	local player = objects.player
+	local pGridCoords = player:getGridCoords()
 	for id, field in pairs(objects.fields) do 
 		fGridCoords = field:getGridCoords()
 		if fGridCoords.x == pGridCoords.x and fGridCoords.y == pGridCoords.y then
-			data = field.fixture:getUserData() 
+			local data = field.fixture:getUserData() 
 			if data == "Red" then 
 				world:setGravity(0,-9.81*tileSize)
 				player:setControls("red")
@@ -39,15 +39,15 @@ function love.update(dt)
 			elseif data == "Violet" then
 			end
 		end
-
 	end	
+
 	if love.keyboard.isDown("left") then 
 		player:moveLeft()
 	end	
 	if love.keyboard.isDown("right") then
 		player:moveRight()
 	end 
-	if love.keyboard.isDown("up") and numColl > 0 then 
+	if love.keyboard.isDown("up") then 
 		player:jump()
 	end
 end
@@ -61,7 +61,8 @@ function love.load()
 	objects.fields = {}
 	for x,y,tile in map("Tile Layer 1"):iterate() do
 		if tile.properties.name == "floor" then
-			Barrier:new(world, x*tileSize, y*tileSize, tileSize, tileSize, "Floor")
+			objects.ground[#objects.ground+1] = Barrier:new(world, x*tileSize, y*tileSize, tileSize, tileSize, "Floor")
+			
 		end
 		if tile.properties.name == "wall" then
 			Barrier:new(world, x*tileSize, y*tileSize, tileSize, tileSize, "Wall")	
@@ -80,23 +81,26 @@ function love.load()
  end
 
 function beginContact(a, b, coll)
-	numColl = numColl + 1
-	
-
-end
-function endContact(a, b, coll)
-	numColl = numColl - 1
-	if numColl == 0 then 
-		canJump = false
+	--print(a:getBody():getLinearVelocity())
+	local player = objects.player
+	player:addCollision()
+	if player:onGround(coll:getNormal()) then
+		player:setCanJump(true)
 	end
 end
-
-function preSolve(a, b, coll)
-	nx, ny = coll:getNormal()
-	if(nx < -30 or nx > 30) then 
-		canJump = false
-	else
+function endContact(a, b, coll)
+	local player = objects.player
+	player:removeCollision()
+	if not player:hasCollisions() then
+		player:setCanJump(false)
+	end
 end
-
-function postSolve(a, b, coll)
+function preSolve(a,b,coll)
+	--This is a fix for when the player would get stuck on flat ground
+	local player = objects.player
+	if(player:againstWall(coll:getNormal())) then
+		if not (love.physics.getDistance(a,b) == 0) then
+			coll:setEnabled(false)
+		end
+	end
 end
--- player.lua
+++ player.lua
@@ -1,42 +1,49 @@
 Player = {}
 tileSize = 32
-moveLeft = {x = -400, y = 0}
-moveRight = {x = 400, y = 0}
-jump = {x = 0, y = -100}
-
+local force = 400
 function Player:new(world, x, y)
+
 	body = love.physics.newBody(world, x, y, "dynamic")
-	shape = love.physics.newRectangleShape(tileSize,tileSize)
+	shape = love.physics.newRectangleShape(32,32)
 	fixture = love.physics.newFixture(body, shape, 1)
-	fixture:setRestitution(0.1)
+	fixture:setRestitution(0.0)
 	fixture:setFriction(0.1)
 	fixture:setUserData("Player")
 	fixture:setMask(7)
 	fixture:setCategory(8)
+	body:setFixedRotation(true)
 	local p = {
 		body = body,
 		shape = shape,
 		fixture = fixture,
+		leftForces = {x = -force, y = 0},
+		rightForces = {x = force, y = 0},
+		jumpForces = {x = 0, y = -force/2},
+		numColls = 0,
+		canJump = true
 	}
 	setmetatable(p, {__index = Player})
 	return p
 end
 function Player:draw()
 	love.graphics.setColor(255,255,255)
-	love.graphics.rectangle("fill", self.body:getX(), self.body:getY(), tileSize , tileSize)
+	love.graphics.rectangle("fill", self.body:getX(), self.body:getY(), 32, 32)
 	love.graphics.setColor(love.graphics.getBackgroundColor())
 end
 function Player:moveLeft()
-	self.body:applyForce(moveLeft.x, moveLeft.y)
+	self.body:applyForce(self.leftForces.x, self.leftForces.y)
 end
 function Player:moveRight()
-	self.body:applyForce(moveRight.x, moveRight.y)
+	self.body:applyForce(self.rightForces.x, self.rightForces.y)
 end
 function Player:jump()
-	self.body:applyLinearImpulse(jump.x, jump.y)
+	if(self.canJump) then 
+		self.canJump = false
+		self.body:applyLinearImpulse(self.jumpForces.x, self.jumpForces.y)
+	end
 end
 function Player:getGridCoords()
-	c = {
+	local c = {
 		x = math.ceil((self.body:getX()-(tileSize/2)) / tileSize) + 1,
 		y = math.ceil((self.body:getY()-(tileSize/2)) / tileSize) + 1
 	}
@@ -44,25 +51,64 @@ function Player:getGridCoords()
 end
 function Player:setControls(s)
 	if s == "red" then
-		setRightMove(400,0)
-		setLeftMove(-400,0)
-		setJump(0,100)
+		self:setRightMove(force,0)
+		self:setLeftMove(-force,0)
+		self:setJump(0,force/2)
 	elseif s == "orange" then
-		setRightMove(0,400)
-		setLeftMove(0,-400)
-		setJump(-100,0)
+		self:setRightMove(0,force)
+		self:setLeftMove(0,-force)
+		self:setJump(-force/2,0)
+	elseif s == "yellow" then
+		self:setRightMove(0, -force)
+		self:setLeftMove(0, force)
+		self:setJump(force/2, 0)
+	elseif s == "green" then
+		self:setRightMove(force, 0)
+		self:setLeftMove(-force, 0)
+		self:setJump(0, -force/2)
 	end
 end
-function setRightMove(x, y)
-	moveRight.x = x
-	moveRight.y = y
+function Player:addCollision()
+	self.numColls = self.numColls + 1
 end
-function setLeftMove(x, y)
-	moveLeft.x = x
-	moveLeft.y = y
+function Player:removeCollision()
+	self.numColls = self.numColls - 1
 end
-function setJump(x, y)
-	jump.x = x
-	jump.y = y
+function Player:onGround(nx, ny)
+	local status = false
+	local xDir = math.abs(self.jumpForces.x) / self.jumpForces.x 
+	local yDir = math.abs(self.jumpForces.y) / self.jumpForces.y
+	if yDir < 0 then 
+		status = ny > 28
+	elseif yDir > 0 then status = ny < -28
+	elseif xDir < 0 then status = nx > 28
+	elseif xDir > 0 then status = ny < -28
+	end
+	return status
+end
+function Player:againstWall(nx, ny)
+	local status = false
+	local xDir = math.abs(self.rightForces.x) / self.rightForces.x
+	local yDir = math.abs(self.rightForces.y) / self.rightForces.y
+	status = nx == 32 or nx == -32 or ny == 32 or ny == -32
+	return status
+end
+function Player:setCanJump(x)
+	self.canJump = x
+end
+function Player:hasCollisions()
+	return self.numColls > 0
+end
+function Player:setRightMove(x, y)
+	self.rightForces.x = x
+	self.rightForces.y = y
+end
+function Player:setLeftMove(x, y)
+	self.leftForces.x = x
+	self.leftForces.y = y
+end
+function Player:setJump(x, y)
+	self.jumpForces.x = x
+	self.jumpForces.y = y
 end
 return Player
